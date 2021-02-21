2d33ffccda9a667636a193fca4cb7ab095c826c9
Guillermo Ojea Quintana <bill_notgates@yahoo.com>
2021-02-21 12:24:08 -0300

    Update release version
    The release version is the same as the old one.

--- io_ogre/__init__.py
+++ io_ogre/__init__.py
@@ -19,7 +19,7 @@ import os, sys, logging
 bl_info = {
     "name": "OGRE Exporter (.scene, .mesh, .skeleton) and RealXtend (.txml)",
     "author": "Brett, S.Rombauts, F00bar, Waruck, Mind Calamity, Mr.Magne, Jonne Nauha, vax456, Richard Plangger, Pavel Rojtberg",
-    "version": (0, 7, 1),
+    "version": (0, 7, 2),
     "blender": (2, 7, 5),
     "location": "File > Export...",
     "description": "Export to Ogre xml and binary formats",
