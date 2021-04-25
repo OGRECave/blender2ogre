65cfaa076d15d3bd2e35ec97344a8cd5b6e4bcb1
sercero <ghojea@gmail.com>
2021-04-25 16:19:59 -0300

    Node Animation requires initial scale as well (master-sync)

--- io_ogre/ogre/skeleton.py
+++ io_ogre/ogre/skeleton.py
@@ -223,7 +223,7 @@ class Keyframe:
         
 # Bone_Track
 # Encapsulates all of the key information for an individual bone within a single animation,
-# and srores that information as XML.
+# and stores that information as XML.
 class Bone_Track:
     def __init__(self, bone):
         self.bone = bone
