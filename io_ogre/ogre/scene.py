583b9cd5f5ad7ba72718da9e0ceb06fd161fbcce
sercero <ghojea@gmail.com>
2021-06-15 22:22:48 -0300

    Set origin of merged objects through dupli_offset

--- io_ogre/ogre/scene.py
+++ io_ogre/ogre/scene.py
@@ -145,12 +145,23 @@ def dot_scene(path, scene_name=None):
     for rem in mobjects:
         if rem in objects:
             objects.remove( rem )
-            exported_meshes.append(rem.data.name)
+            exported_meshes.append( rem.data.name )
 
     for group in mgroups:
         merged = merge_group( group )
         objects.append( merged )
         temps.append( merged )
+
+        # If user has set an offset for the dupli_group, then use that to set the origin of the merged objects
+        if group.dupli_offset != mathutils.Vector((0.0, 0.0, 0.0)):
+            logger.info("Change origin of merged object %s to: %s" % ( merged.name, group.dupli_offset ))
+            
+            # Use the 3D cursor to set the object origin
+            merged.select = True
+            saved_location = bpy.context.scene.cursor_location  # Save 3D cursor location
+            bpy.context.scene.cursor_location = group.dupli_offset
+            bpy.ops.object.origin_set(type='ORIGIN_CURSOR')     # Set the origin on the current object to the 3D cursor location
+            bpy.context.scene.cursor_location = saved_location  # Set 3D cursor location back to the stored location
 
     # Gather roots because ogredotscene supports parents and children
     def _flatten( _c, _f ):
@@ -203,7 +214,7 @@ def dot_scene(path, scene_name=None):
     for ob in temps:
         logger.debug("Removing temporary mesh: %s" % ob.data.name)
         bpy.data.meshes.remove(ob.data)
-        logger.debug("& Removing temporal object: %s" % ob.name)
+        logger.debug("Removing temporal object: %s" % ob.name)
         bpy.data.objects.remove(ob, do_unlink=True)
         #bpy.context.scene.objects.unlink( ob )
 
